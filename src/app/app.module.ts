94b284f60334bba96ec62d9bd7d96f6edd59ef8d
Kareemelshekh11 <karemelsheah1998@gmail.com>
2023-02-21 19:44:01 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './Components/slider/slider.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
