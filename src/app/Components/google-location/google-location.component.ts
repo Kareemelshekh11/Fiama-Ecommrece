d4d607aedcfe4187dc5a2cf00beb96c23586891e
Kareemelshekh11 <karemelsheah1998@gmail.com>
2023-02-27 19:21:57 +0200

    changes

---
+++ src/app/Components/google-location/google-location.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-location',
+  templateUrl: './google-location.component.html',
+  styleUrls: ['./google-location.component.scss']
+})
+export class GoogleLocationComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,47 @@ import { AppComponent } from './app.component';
 import { SliderComponent } from './Components/slider/slider.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutComponent } from './Components/about/about.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { PortfolioDetailsComponent } from './Components/portfolio-details/portfolio-details.component';
+import { GoogleLocationComponent } from './Components/google-location/google-location.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { ComingSoonComponent } from './Components/coming-soon/coming-soon.component';
+import { ShopComponent } from './Components/shop/shop.component';
+import { ShopGridComponent } from './Components/shop-grid/shop-grid.component';
+import { ShopDetailsComponent } from './Components/shop-details/shop-details.component';
+import { NewsComponent } from './Components/news/news.component';
+import { NewsGridComponent } from './Components/news-grid/news-grid.component';
+import { NewsDetailsComponent } from './Components/news-details/news-details.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { CheckoutComponent } from './Components/checkout/checkout.component';
+import { BrandsComponent } from './Components/brands/brands.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    PortfolioComponent,
+    PortfolioDetailsComponent,
+    GoogleLocationComponent,
+    NotfoundComponent,
+    ComingSoonComponent,
+    ShopComponent,
+    ShopGridComponent,
+    ShopDetailsComponent,
+    NewsComponent,
+    NewsGridComponent,
+    NewsDetailsComponent,
+    ContactComponent,
+    CartComponent,
+    CheckoutComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule,
